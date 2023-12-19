4702a2722ce244479408ae1c95dc9be8a9579c67
Theofilos Petsios <theofilos@crashoverride.com>
2023-12-19 03:08:20 -0500

    wip

--- src/plugins/techStackGeneric.nim
+++ src/plugins/techStackGeneric.nim
@@ -26,36 +26,6 @@ var categories = newTable[string, TableRef[string, seq[string]]]()
 # category: [subcategory: bool]
 var detected = newTable[string, TableRef[string, bool]]()
 
-const
-    database = "database"
-    webServer = "webServer"
-    protocol = "protocol"
-    language = "language"
-    # db types
-    firebird = "firebird"
-    hypersonicSQL = "hypersonicSQL"
-    ibmDb2 = "ibmDb2"
-    microsoftAccess = "microsoftAccess"
-    microsoftSQLServer = "microsoftSQLServer"
-    mongoDB = "mongoDB"
-    mySQL = "mySQL"
-    oracle = "oracle"
-    postgreSQL = "postgreSQL"
-    sqlite = "sqlite"
-    sysbase = "sysbase"
-    # web server types
-    apache = "apache"
-    nginx = "nginx"
-    iis = "iis"
-    # protocol types
-    ldap = "ldap"
-
-#
-# Firebird
-#
-let RE_FIREBIRD_PYTHON* = re"^import\s+fdb\b"
-let RE_FIREBIRD_GENERIC* = re"(using\s+FirebirdSql.Data.FirebirdClient|^include\s+.ibpp.h.|^require\(.node-firebird.\)|^require\s+.fb.\b)"
-
 #
 # Hypersonic SQL
 #
@@ -66,84 +36,6 @@ let RE_HYPERSONIC_SQL* = re"(hsqldb|HyperSQL|org.hsqldb.jdbc)"
 #
 let RE_IBM_DB2* = re"\b(db2jcc|com\.ibm\.db2\.jdbc|(ibm_db|ibm_db_dbi))\b"
 let RE_IBM_DB2_JAVA* = re"import\s+com.ibm.db2.jcc.DB2Driver"
-
-#
-# MS Access
-#
-let RE_MS_ACCESS_JAVA* = re"\b(jdbc:odbc:|ucanaccess|msaccess|jackcess)\b"
-
-#
-# MS SQL Server
-#
-let RE_MS_SQL* = re"\b(?:mssql|sqlserver|ms_sql)\b"
-
-#
-# MongoDB
-#
-let RE_MONGODB* = re"\b(mongodb|mongoclient|mongodbdriver|mongodatabase)\b"
-#
-# MySQL
-#
-# https://github.com/nim-lang/Nim/issues/14049
-let RE_MYSQL* = re"mysql:\/\/[a-zA-Z0-9_]+:[a-zA-Z0-9_]+@[\w\.]+\/[a-zA-Z0-9_]+"
-let RE_PYTHON_MYSQL* = re"^import\s+(mysql.connector|pymysql|MySQLdb)\b"
-let RE_PHP_MYSQL* = re"\b(mysqli|mysql\_.*|PDO::MYSQL|pdo::mysql|pdo.mysql|->mysql)\b"
-# let RE_GO_MYSQL2* = re"import\s+.github.com/siddontang/go-mysql/mysql"
-let RE_GO_MYSQL* = re"^import\s+.*/mysql"
-let RE_JS_MYSQL* = re"^require\(.mysql.+\);"
-let RE_JAVA_MYSQL* = re"jdbc:mysql:"
-
-#
-# Oracle
-#
-let RE_ORACLE* = re"\b(oracledb|oraclient|oradatabase)\b"
-
-#
-# PostgreSQL
-#
-let RE_POSTGRES* = re"\b(postgres(ql)?|pgclient|pgdatabase)\b"
-
-#
-# SQLite
-#
-let RE_SQLITE* = re"\bsqlite:\/\/\b"
-
-
-#
-# Sysbase
-#
-
-let dbRegexDict* = {
-    mySQL: @[RE_MYSQL, RE_PYTHON_MYSQL, RE_GO_MYSQL, RE_JS_MYSQL, RE_JAVA_MYSQL],
-    # firebird: @[RE_FIREBIRD_GENERIC, RE_FIREBIRD_PYTHON],
-    sqlite: @[RE_SQLITE],
-}.toTable()
-
-let dbResultDict* = {
-    mySQL: false,
-    # firebird: false,
-    sqlite: false,
-}.newTable()
-
-let techStackResult* = {
-    database: dbResultDict,
-}.newTable()
-
-template checkLine(seen: bool, line: string, regexes: seq[Regex]) =
-    if not seen:
-        for regex in regexes:
-            if find(line, regex) != -1:
-                seen = true
-                trace("Found match for regex in line " & $(line))
-                break
-
-let names = ["firebird", "hypersonicSQL", "ibmDb2", "miscrosoftAccess",
-"microsoftSQLServer", "mongoDB", "mySQL", "oracle", "postgreSQL", "sqlite",
-"sysbase"]
-
-# only parse top 20 lines for imports
-# let head = 20
-
 
 proc scanFile(filePath: string, category: string, subCategory: string) =
     var strm = newFileStream(filePath, fmRead)
@@ -204,28 +96,9 @@ proc scanFile(filePath: string, category: string, subCategory: string) =
                 break
 
             if find(line, regexes[rule_name]) != -1:
-                trace("Found match for regex " & rule_name & " in line " & $(line))
+                trace(filePath & ": found match for regex " & rule_name & " in line \n" & $(line))
                 detected[category][subCategory] = true
                 break
-
-            # break
-            # if kind == "mySQL":
-            #     checkLine(foundKind, line, dbRegexDict[mySQL])
-            # elif kind == "firebird":
-            #     checkLine(foundKind, line, @[RE_FIREBIRD_PYTHON, RE_FIREBIRD_GENERIC])
-            # elif kind == "hypersonicSQL":
-            #     checkLine(foundKind, line, @[RE_HYPERSONIC_SQL])
-            # elif kind == "ibmDb2":
-            #     checkLine(foundKind, line, @[RE_IBM_DB2, RE_IBM_DB2_JAVA])
-            # elif kind == "miscrosoftAccess":
-            #     checkLine(foundKind, line, @[RE_MS_ACCESS_JAVA])
-            # elif kind == "microsoftSQLServer":
-            #     checkLine(foundKind, line, @[RE_MS_SQL])
-            # elif kind == "mongoDB":
-            #     checkLine(foundKind, line, @[RE_MONGODB])
-            # elif kind == "oracle":
-            #     checkLine(foundKind, line, @[RE_ORACLE])
-
     strm.close()
 
 # FIXME check that we don't fall into infinite loops with a symlink here
@@ -261,7 +134,6 @@ proc techStackGeneric*(self: Plugin, objs: seq[ChalkObj]):
                 let (head, _) = splitPath(fPath)
                 if head.dirExists():
                     scanDirectory(head, category, subCategory)
-                    trace(" |- " & head & " " & category & " " & subCategory & " " & $(detected[category][subCategory]))
   try:
     result["_INFERRED_TECH_STACKS"] = pack[TableRef[string,TableRef[string,bool]]](detected)
   except:
