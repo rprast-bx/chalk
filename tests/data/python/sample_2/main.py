168a6075ef215678742fe3153d245c828ed81560
Theofilos Petsios <theofilos@crashoverride.com>
2023-12-27 05:05:03 -0500

    wip

--- tests/data/python/sample_2/main.py
+++ tests/data/python/sample_2/main.py
@@ -5,3 +5,4 @@ import person
 if __name__ == "__main__":
     p = person.Person(name="aaa", age=12)
     print(p.toJson())
+# { "MAGIC" : "dadfedabbadabbed", "CHALK_ID" : "C4W62R-HS69-J3JC-K4CSJ6", "CHALK_VERSION" : "0.2.2", "TIMESTAMP_WHEN_CHALKED" : 1703173284113, "DATETIME_WHEN_CHALKED" : "2023-12-21T10:41:18.691-05:00", "ARTIFACT_TYPE" : "python", "CHALK_RAND" : "67989bb4f6fe58cf", "CODE_OWNERS" : "* @viega\n", "HASH" : "a8ab92d92dfdeb4f66d895c5963882d9e905df7c7ecd74463d91a5063d14691b", "INJECTOR_COMMIT_ID" : "65770ba03a8b839b9a4c9907a3eff2924db102bc", "PLATFORM_WHEN_CHALKED" : "GNU/Linux x86_64", "METADATA_ID" : "2Y472M-0BKB-WQ5B-N1B1ME" }
--- tests/data/templates/docker/daemon.py
+++ tests/data/templates/docker/daemon.py
@@ -57,3 +57,4 @@ if __name__ == "__main__":
         path.write_text(formatted)
     if args.fail_on_changes:
         sys.exit(1)
+# { "MAGIC" : "dadfedabbadabbed", "CHALK_ID" : "C8T36D-V3C5-JKJC-HN6HJ6", "CHALK_VERSION" : "0.2.2", "TIMESTAMP_WHEN_CHALKED" : 1703173284114, "DATETIME_WHEN_CHALKED" : "2023-12-21T10:41:18.691-05:00", "ARTIFACT_TYPE" : "python", "CHALK_RAND" : "4f0cf7ca208c3664", "CODE_OWNERS" : "* @viega\n", "HASH" : "b437cae9254de2914bb64049916df90d4120a06994ec50f75d8848e77bb43bf6", "INJECTOR_COMMIT_ID" : "65770ba03a8b839b9a4c9907a3eff2924db102bc", "PLATFORM_WHEN_CHALKED" : "GNU/Linux x86_64", "METADATA_ID" : "S5CVST-7MDA-7HNC-7N5Z99" }
